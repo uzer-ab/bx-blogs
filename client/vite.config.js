c76bdd278174acc9801282adfb03abde45042752
uzzair <uzerahmed151@gmail.com>
2025-12-26 06:44:48 +0500

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,41 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   define: {
+//     __API_URL__: JSON.stringify(process.env.API_URL),
+//     __MODE__: JSON.stringify(process.env.MODE),
+//   },
+// });
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import dotenv from "dotenv";
-dotenv.config();
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  define: {
-    __API_URL__: JSON.stringify(process.env.API_URL),
-    __MODE__: JSON.stringify(process.env.MODE),
-  },
+    define: {
+      __API_URL__: JSON.stringify(env.API_URL),
+      __MODE__: JSON.stringify(env.MODE),
+    },
+  };
 });
